276b55d822ffa48df3358daa167ec8c0a728935b
Fire160 <michaelslashfire@gmail.com>
2015-11-21 19:45:35 -0700

    Adding movement to do the thing

---
+++ Text-Based Monopoly/src/Movement.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Movement
+	{
+	public static void playerMove(ArrayList<Player> players,int player, ArrayList<Square> b)
+		{
+		int dieO = (int) (Math.random()*6) + 1;
+		int dieT = (int) (Math.random()*6) + 1;
+		int roll = dieO + dieT;
+		int doubles = 0;
+		System.out.println("You rolled a " + dieO + " and a " + dieT + " for a total of " + roll);
+		if(dieO == dieT)
+			{
+			doubles++;
+			}
+		for(int i = 0; i < b.size(); i++)
+			{
+			if(players.get(player).getSpot() >= b.size())
+				{
+				players.get(player).getSpot() = 0;
+				}
+			else
+				{
+				players.get(player).setSpot(players.get(player).getSpot() + 1);
+				}
+			}
+		System.out.println("You landed on " + b.get(players.get(player).getSpot()).getName());
+		}
+	}
