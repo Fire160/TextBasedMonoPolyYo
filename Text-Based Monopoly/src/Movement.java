36aa8d4aa22ab8e828119c1a8bed0b3e3722cf16
Michael Young <michael.young@MU-C1-08.mullen.pvt.k12.co.us>
2015-12-01 09:09:02 -0700

    Star Wars Changing in progress

--- Text-Based Monopoly/src/Movement.java
+++ Text-Based Monopoly/src/Movement.java
@@ -7,10 +7,11 @@ public class Movement
 		int doubles = 0;
 		for(int g =0; g < players.size(); g++)
 			{
-			System.out.println("+-----(PLAYER " + (g+1) + "'s TURN)-----+");
+			System.out.println("+-----(" + players.get(g).getPlayerName() + "'s Turn)-----+");
 			System.out.println("Your current money: " + players.get(g).getMoney());
 			System.out.println("Your current Properties: " + players.get(g).getProperties());
 			System.out.println("You are on "  + b.get(players.get(g).getSpot()).getName() + ". Press anything to roll the dice!");
+			Runner.things.nextLine();
 			Runner.things.nextLine();
 			int dieO = (int) (Math.random()*6) + 1;
 			int dieT = (int) (Math.random()*6) + 1;
@@ -38,7 +39,6 @@ public class Movement
 				}
 			System.out.println("You landed on " + b.get(players.get(g).getSpot()).getName());
 			b.get(players.get(g).getSpot()).runSquare(players, g, b, players.get(g).getSpot());
-			Runner.things.nextLine();
 			}
 		}
 	}
