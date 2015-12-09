c886b6d82bdeb2a13f554e6533a44da49854c981
Michael Young <michael.young@MU-C1-08.mullen.pvt.k12.co.us>
2015-12-09 14:06:48 -0700

    Stuff file kinda

--- Text-Based Monopoly/src/Runner.java
+++ Text-Based Monopoly/src/Runner.java
@@ -1,10 +1,11 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Runner
 	{
 	public static Scanner things = new Scanner(System.in);
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 		{
 		ArrayList<Square> board = ArrayMakers.makeBoard();
 		ArrayList<Player> persons = ArrayMakers.makePlayers();
